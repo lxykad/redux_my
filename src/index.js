851036cae1600662e0962d65531a543e965b450b
1 <a@1deiMac-4.local>
2017-12-29 17:53:42 +0800

    add redux

--- src/index.js
+++ src/index.js
@@ -4,47 +4,38 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import Store from './store/Store'
 
-/*
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
-*/
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 
-/*
-* state 被修改的对象
-* stateChanger 描述数据变化的函数
-* */
-function createStore(state, stateChanger) {
-    const listeners = []
-    const subscribe = (listener) => listeners.push(listener)
-
-    const getState = () => state
-    const dispatch = (action) => {
-        stateChanger(state, action)
-        listeners.forEach(listener => listener())
-    }
-    return {getState, dispatch, subscribe}
-}
 
 /*
 * 专门负责数据的 函数
 * state 被修改的对象
 * action 修改的值的对象
 * */
-function stateChanger(state, action) {
+const stateChanger = (action) => {
 
     switch (action.type) {
         case 'UPDATE_TITLE_TEXT':
-            state.title.text = action.text
+            appState.title.text = action.text
             break
         case 'UPDATE_TITLE_COLOR':
-            state.title.color = action.color
+            appState.title.color = action.color
             break
         default:
             break
     }
 }
+
+function createTitleAction(str) {
+
+    return{
+        type:'UPDATE_TITLE_TEXT',
+        text:str
+    }
+}
+
 
 const appState = {
     title: {
@@ -77,11 +68,13 @@ function renderContent(content) {
 renderApp(appState)
 
 
-let store = createStore(appState, stateChanger);
-store.subscribe(()=>renderApp(store.getState()))
+let store = createStore(stateChanger);
+
+//let action = createTitleAction('555');
+
+//console.log(createTitleAction('555'))
+//store.dispatch(createTitleAction('555'))
 
 
-store.dispatch({type: 'UPDATE_TITLE_TEXT', text: '修改后的title'})
 
-// 把新的数据渲染到页面上
-//renderApp(store.getState())
+//store.dispatch(appState,{type: 'UPDATE_TITLE_TEXT', title: 'haha'})
